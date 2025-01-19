7bad5f0192a9fb29a07b31d05a862d1e94841070
Karol Swirniak <karol.swirniak@protonmail.com>
2025-01-19 14:42:54 +0100

    deleted redundant package spring/domain/repository

--- src/main/java/swirn/spring/domain/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package swirn.spring.domain.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import swirn.spring.domain.entity.Book;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-
-}
