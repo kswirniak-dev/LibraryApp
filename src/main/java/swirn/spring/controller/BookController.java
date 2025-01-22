ac1c101943b4cd27fa8104183c6b138d54b98074
Karol Swirniak <karol.swirniak@protonmail.com>
2025-01-22 17:49:52 +0100

    Deletion functionality iniitially implemented.

--- src/main/java/swirn/spring/controller/BookController.java
+++ src/main/java/swirn/spring/controller/BookController.java
@@ -3,7 +3,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.validation.Valid;
@@ -46,4 +48,10 @@ public class BookController {
 	    bookRepository.save(book);
 	    return "redirect:/index";	
 		}
+	
+	@DeleteMapping("deletebook/{id}")
+    public String delete(@PathVariable("id") Long id){
+		bookRepository.deleteById(id);        
+        return "redirect:/index";
+	}
 }
