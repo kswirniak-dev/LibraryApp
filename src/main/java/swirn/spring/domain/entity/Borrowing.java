2947ef4758ba93e31f027147bc2254deeb61d603
Karol Swirniak <karol.swirniak@protonmail.com>
2025-01-22 11:53:54 +0100

    Minor entities issues fixed

--- src/main/java/swirn/spring/domain/entity/Borrowing.java
+++ src/main/java/swirn/spring/domain/entity/Borrowing.java
@@ -9,24 +9,24 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter @NoArgsConstructor
+@Table
 public class Borrowing {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
-	@Column
 	@OneToOne
 	@JoinColumn(name = "book_id")
 	private Book book;
 	
-	@Column
 	@OneToOne
 	@JoinColumn(name = "holder_id")
 	private Holder holder;
