3d654a837623e83cdce1be4eb220e64ba3acf58f
Karol Swirniak <karol.swirniak@protonmail.com>
2025-01-27 15:05:49 +0100

    Changed BookRepository to JpaRepository type

--- src/main/java/swirn/spring/repository/BookRepository.java
+++ src/main/java/swirn/spring/repository/BookRepository.java
@@ -1,11 +1,12 @@
 package swirn.spring.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import swirn.spring.domain.entity.Book;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
