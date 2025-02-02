90d2fb3e52744ddae30c988232cbb411d05c341f
Karol Swirniak <karol.swirniak@protonmail.com>
2025-02-02 12:51:37 +0100

    Changed RentralRepository to JpaRepository type

--- src/main/java/swirn/spring/repository/RentalRepository.java
+++ src/main/java/swirn/spring/repository/RentalRepository.java
@@ -1,11 +1,12 @@
 package swirn.spring.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import swirn.spring.domain.entity.Rental;
 
 @Repository
-public interface RentalRepository extends CrudRepository<Rental, Long> {
+public interface RentalRepository extends JpaRepository<Rental, Long> {
 
 }
