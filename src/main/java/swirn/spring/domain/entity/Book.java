a49dd5d4239d728f0f01e16b27151bb919e7c64c
Karol Swirniak <karol.swirniak@protonmail.com>
2025-02-06 19:56:09 +0100

    added holders to all books view

--- src/main/java/swirn/spring/domain/entity/Book.java
+++ src/main/java/swirn/spring/domain/entity/Book.java
@@ -1,12 +1,14 @@
 package swirn.spring.domain.entity;
 
 import java.time.Year;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,5 +35,10 @@ public class Book {
 	@Column(columnDefinition = "YEAR", nullable = false)
 	@Getter @Setter
 	private Year year;
+	
+	@Column
+	@OneToMany(mappedBy = "book")
+	@Getter @Setter
+	private List<Rental> rentals;
 
 }
