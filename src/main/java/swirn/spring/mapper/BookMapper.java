278803f7257f8da7ab268f7c2e1b1185133af555
Karol Swirniak <karol.swirniak@protonmail.com>
2025-02-16 18:43:51 +0100

    BookMapper implemented

---
+++ src/main/java/swirn/spring/mapper/BookMapper.java
@@ -0,0 +1,23 @@
+package swirn.spring.mapper;
+
+
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import swirn.spring.domain.entity.Book;
+import swirn.spring.dto.BookDTO;
+
+@Mapper
+public interface BookMapper {
+
+    BookDTO bookToBookDTO(Book entity);
+
+    Book bookDTOtoBook(BookDTO dto);
+    
+    List<Book> convertBookDTOListToBookList(List<BookDTO> list);
+
+    List<BookDTO> convertBookListToBookDTOList(List<Book> list);
+}
