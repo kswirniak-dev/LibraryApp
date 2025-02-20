cc3af6616fc0ad06d250d87e19cf51b2cbf79bb1
Karol Swirniak <karol.swirniak@protonmail.com>
2025-02-20 20:59:46 +0100

    RentalServiceImpl implemented

--- src/main/java/swirn/spring/service/impl/RentalServiceImpl.java
+++ src/main/java/swirn/spring/service/impl/RentalServiceImpl.java
@@ -1,33 +1,57 @@
 package swirn.spring.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import swirn.spring.dto.RentalDTO;
+import swirn.spring.mapper.RentalMapper;
+import swirn.spring.repository.RentalRepository;
 import swirn.spring.service.RentalService;
 
 import java.util.List;
-
+import java.util.stream.Collectors;
+@Service
 public class RentalServiceImpl implements RentalService {
+    private final RentalRepository rentalRepository;
+    private final RentalMapper rentalMapper;
+    @Autowired
+    public RentalServiceImpl(RentalRepository rentalRepository, RentalMapper rentalMapper) {
+        this.rentalRepository = rentalRepository;
+        this.rentalMapper = rentalMapper;
+    }
+
+
     @Override
     public List<RentalDTO> getAll() {
-        return null;
+        return rentalRepository
+                .findAll()
+                .stream()
+                .map(rentalMapper::rentalToRentalDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public RentalDTO getById(Long id) {
-        return null;
+        return rentalMapper.rentalToRentalDTO(rentalRepository.findById(id).orElseThrow(() -> new IllegalArgumentException()));
     }
 
     @Override
     public void deleteById(Long id) {
-
+        rentalRepository.deleteById(id);
     }
 
     @Override
     public RentalDTO create(RentalDTO rental) {
-        return null;
+        return rentalMapper.rentalToRentalDTO(rentalRepository.save(rentalMapper.rentalDTOtoRental(rental)));
     }
 
     @Override
     public RentalDTO update(Long id, RentalDTO rental) {
-        return null;
+        RentalDTO rentalFromRepo = rentalMapper.rentalToRentalDTO(rentalRepository.findById(id).orElseThrow(() -> new IllegalArgumentException()));
+        rentalFromRepo.setBookId(rental.getBookId());
+        rentalFromRepo.setHolder(rental.getHolder());
+        rentalFromRepo.setStartDate(rental.getStartDate());
+        rentalFromRepo.setEndDate(rental.getEndDate());
+        rentalRepository.save(rentalMapper.rentalDTOtoRental(rentalFromRepo));
+        return rentalFromRepo;
     }
 }
