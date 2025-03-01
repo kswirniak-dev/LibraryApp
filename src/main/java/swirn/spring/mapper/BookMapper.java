6fde30c5c4c5029d035fdbf05cf32111900771ee
Karol Swirniak <karol.swirniak@protonmail.com>
2025-03-01 22:30:03 +0100

    refactored BookMapper to provide for Optional

--- src/main/java/swirn/spring/mapper/BookMapper.java
+++ src/main/java/swirn/spring/mapper/BookMapper.java
@@ -3,15 +3,20 @@ package swirn.spring.mapper;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import swirn.spring.domain.entity.Book;
+import swirn.spring.domain.entity.Rental;
 import swirn.spring.dto.BookDTO;
+import swirn.spring.dto.RentalDTO;
 import swirn.spring.dto.simple.BookSimpleDTO;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {RentalMapper.class})
 public interface BookMapper {
 
+    @Mapping(target = "activeRental", expression = "java(mapOptionalRental(entity.getActiveRental()))")
     BookDTO bookToBookDTO(Book entity);
 
     Book bookDTOtoBook(BookDTO dto);
@@ -25,4 +30,10 @@ public interface BookMapper {
     List<Book> convertBookDTOListToBookList(List<BookDTO> list);
 
     List<BookDTO> convertBookListToBookDTOList(List<Book> list);
+
+    default RentalDTO mapOptionalRental(Optional<Rental> rental) {
+        return rental.map(this::rentalToRentalDTO).orElse(null);
+    }
+
+    RentalDTO rentalToRentalDTO(Rental rental);
 }
